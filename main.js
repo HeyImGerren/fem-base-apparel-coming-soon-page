33617562ea2748dc648e1371307e03bde10f2717
Gerren Penaloza <gerren.penaloza@gmail.com>
2023-10-24 13:13:36 -0700

    added error handling on email submit

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const emailField = document.getElementById("email");
 const submitBtn = document.getElementById("submit-btn");
+const errorIcon = document.querySelector(".icon-error");
+const errorMessage = document.querySelector(".email-error-msg");
 
 submitBtn.addEventListener('click', () => {
   if (!emailField.checkValidity()) {
-    console.log('invalid email')
+    // email is not valid so we must display the error icon and message
+    errorIcon.toggleAttribute('data-visible');
+    errorMessage.toggleAttribute('data-visible');
   } else {
-    console.log('valid email');
-  } 
+    // 
+    console.log("valid email");
+  }
 })
 // if (!emailField.checkValidity()) {
 //   console.log('invalid email');
